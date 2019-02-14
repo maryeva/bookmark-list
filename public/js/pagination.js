3e1c911016c2dc695abfec4e127f4adf90d4ff1d
Mary Manousou <mary@Marys-MacBook-Air.local>
2019-02-14 12:39:00 +0000

    moved renderResults function in pagination file

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,7 +1,5 @@
-const renderResults = require('./renderResults');
-
 module.exports = (function calculatePagination(storedLinks) { 
-  var config = {
+  const config = {
     currentPage: 1,
     maxItemsPerPage: 20,
     get totalPages() {
@@ -58,6 +56,33 @@ module.exports = (function calculatePagination(storedLinks) {
     renderNavigation();
     renderResults();
   }
+
+  /**
+  * Render only results needed for each page
+  */
+  function renderResults() {
+    document.getElementById('links').innerHTML = '';
+    for (var id=(config.currentPage-1) * config.maxItemsPerPage; id < (config.currentPage * config.maxItemsPerPage) && id < storedLinks.length; id++) {
+      let li = document.createElement('li');
+      li.innerHTML = `
+        <div class="content">${storedLinks[id]}</div> 
+        <div class="btn-container">
+          <button class="saveBtn" id="${id}">
+            <i class="material-icons">check</i>
+          </button>
+          <button class="editBtn" id="${id}">
+            <i class="material-icons">edit</i>
+          </button>
+          <button class="removeBtn" id="${id}">
+            <i class="material-icons">delete</i>
+          </button>
+        </div>
+      `;
+      document.getElementById('links').appendChild(li);
+    }
+   
+    return document.getElementById('links');
+   }
 
   renderResults();
   renderNavigation();
